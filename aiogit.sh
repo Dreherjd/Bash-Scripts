2f7d0bc386b81624aa98f7b300fda1b892d88e77
Dreherjd <dreherjd71@gmail.com>
2025-01-23 14:34:04 -0500

    added more comments

--- aiogit.sh
+++ aiogit.sh
@@ -1,9 +1,12 @@
 # asks user for a commit message and pushes up current git directory
-
 #!/bin/bash
+# get current directory
 DIR=$(pwd)
+#cd into it
 cd "$DIR"
+#get the commit message from the user
 read -p "Git commit Message: " COMMIT
+#do the git stuff
 git add .
 git commit -m "$COMMIT"
 git push
